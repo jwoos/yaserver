7ca5f8ef3a3d0f43bbef193782c487552b000a6d
Jun Woo Shin <shin.junwoo97@gmail.com>
2020-04-06 21:54:08 -0700

    prototype request parsing

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,15 +1,25 @@
 use crate::http;
+use std::vec;
+use std::collections::HashMap;
 
 pub struct Request {
     method: http::HTTPMethod,
-    data: std::vec::Vec<u8>,
+    data: vec::Vec<u8>,
+    headers: HashMap<String, String>,
 }
 
 impl Request {
-    pub fn new(method: http::HTTPMethod, data: std::vec::Vec<u8>) -> Request {
+    pub fn new(method: http::HTTPMethod, data: vec::Vec<u8>) -> Request {
+        let hashmap: HashMap<String, String> = HashMap::new();
         return Request {
-            method,
-            data,
+            method: method,
+            data: data,
+            headers: hashmap,
         };
+    }
+
+    pub fn parse(data: vec::Vec<u8>) -> (HashMap<String, String>, String) {
+        let mut headers: HashMap<String, String> = HashMap::new();
+        return (headers, "".to_string());
     }
 }
