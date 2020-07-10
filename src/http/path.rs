36d06b8f1d2fb769fbd86f35f8f406cd33791e40
Jun Woo Shin <shin.junwoo97@gmail.com>
2020-07-09 21:46:09 -0700

    basic path with literals

--- src/http/path.rs
+++ src/http/path.rs
@@ -1,9 +1,35 @@
+use std::fmt;
+use std::rc::Rc;
 use std::vec::Vec;
 
-enum Token {
-    Literal(Lit),
-    Regex(regex),
-    Pattern(pattern),
+pub enum Token {
+    Invalid,
+    Literal(String),
+    Pattern(String),
+    // TODO support?
+    Regex(String),
+}
+
+pub struct PathError {
+    message: String,
+}
+
+impl PathError {
+    pub fn new(message: String) -> PathError {
+        return PathError { message };
+    }
+}
+
+impl fmt::Display for PathError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        return write!(formatter, "{}", self.message);
+    }
+}
+
+impl fmt::Debug for PathError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        return write!(formatter, "PathError {{ message: {} }}", self.message,);
+    }
 }
 
 pub struct Path {
@@ -12,10 +38,43 @@ pub struct Path {
 }
 
 impl Path {
-    fn new(literal: String) {
+    // TODO
+    fn validate(literal: &str) -> bool {
+        return false;
+    }
+
+    pub fn new() -> Path {
         return Path {
-            literal,
-            Vec::new(),
+            literal: String::from(""),
+            tokens: Vec::new(),
         };
+    }
+
+    // TODO the string has to be decoded from URL encoded form before returning a token
+    pub fn parse(literal: String) -> Result<Path, PathError> {
+        // TODO support anything other than literals
+        let tokens: Vec<_> = literal
+            .split("/")
+            .map(|tok| Token::Literal(String::from(tok)))
+            .collect();
+        return Ok(Path { literal, tokens });
+
+        /*
+         *        for tok in tokens_iter {
+         *            // The minimum a non literal can be is 4 since <r:tok> or <p:tok>
+         *            if tok.len() < 5 {
+         *                tok_vec.append(Token::Literal(tok));
+         *            }
+         *
+         *            // URL validation
+         *            if !Path::validate(tok) {
+         *                return PathError::new("Invalid path");
+         *            }
+         *
+         *            let tok: String = tok.split(|c: char| -> bool {
+         *                return c == '<' || c == '>';
+         *            }).collect::<String>();
+         *        }
+         */
     }
 }
